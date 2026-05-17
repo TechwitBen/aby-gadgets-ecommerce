85e9c6111b673ab44ee1f8e642e3cf608365e83f
Ben <benedictleo34@gmail..com>
2026-05-17 19:01:17 +0100

    Fix contact controller import casing

--- backend/routes/contact.routes.js
+++ backend/routes/contact.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { contactFormController } from  "../controllers/contact.Controller.js";
+import { contactFormController } from  "../controllers/contact.controller.js";
  
 const contactRouter = Router();
  
